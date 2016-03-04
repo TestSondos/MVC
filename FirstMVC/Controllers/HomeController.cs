7aee365dd31a3d6ec38b4cc5db62d03d29b0fb20
Sondos <sondos379@gmail.com>
2016-03-05 00:59:06 +0200

    Adding my first Asp.NET MVC application

---
+++ FirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Description page of PHR";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
