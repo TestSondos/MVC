1d949f00bddc26fd33284fd0cb615f7405286107
Sondos <sondos379@gmail.com>
2016-03-14 15:26:23 +0200

    Add All patients view

---
+++ FirstMVC/Controllers/PatientsController.cs
@@ -0,0 +1,110 @@
+﻿using FirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class PatientsController : Controller
+    {
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            var model = from r in reviews
+                        orderby r.address
+                        select r;
+            return View(model);
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reviews/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reviews/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        static List<Patients> reviews = new List<Patients>
+        {
+            new Patients
+            {
+                id = 1,
+                name = "Sondos Sami",
+                phone = "011111",
+                address = "Al Haram"
+            },
+            new Patients
+            {
+                id = 2,
+                name = "Aya Hamdy",
+                phone = "0122222",
+                address = "Helwan"
+            }
+        };
+    }
+}
---
+++ FirstMVC/Models/Patients.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVC.Models
+{
+    public class Patients
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string  phone{ get; set; }
+        public string address { get; set; }
+
+    }
+}
