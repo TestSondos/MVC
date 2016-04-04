0bc9a23b73d265c420cd46b1effc0db2fc2aaf38
Sondos Sami <sondos379@gmail.com>
2016-04-04 10:08:40 +0200

    add datastore and helper class

---
+++ DataLayer/DataStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class DataStore
+    {
+        public List<string> GetMedicalHistory(string id)
+        {
+            switch (id)
+            {
+                case "b48f659d-6d5a-4a98-abed-1d4cd0a9966d":
+                    return new List<string> { "A", "B", "C" };
+                default:
+                    return new List<string> { "No Medical History" };
+            }
+        }
+    }
+}
---
+++ FirstMVC/Controllers/MedRecController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _360Courier.Extensions;
+using System.Security.Claims;
+using FirstMVC.Helpers;
+using DataLayer;
+
+namespace FirstMVC.Controllers
+{
+    public class MedRecController : Controller
+    {
+        // GET: MedRec
+        public ActionResult Index()
+        {
+            var dataStore = new DataStore();
+            ViewBag.UserData = dataStore.GetMedicalHistory(Helper.Instance.UserId).ToJson();
+            return View();
+        }
+
+        // GET: MedRec/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MedRec/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MedRec/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MedRec/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MedRec/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MedRec/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MedRec/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
