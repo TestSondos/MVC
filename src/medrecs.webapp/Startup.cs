cb60ffea6d9068426cb28185d650b5fd148f7d7a
sondos sami <sondos379@gmail.com>
2016-06-02 03:41:30 +0200

    Add: medrecs webapp

---
+++ src/medrecs.webapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(medrecs.webapp.Startup))]
+namespace medrecs.webapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
