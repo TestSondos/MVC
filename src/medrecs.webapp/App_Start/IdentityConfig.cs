3d16f611dbbf8946d11d39f4c941b0e38e304941
sondos sami <sondos379@gmail.com>
2016-06-04 04:54:06 +0200

    simplify the password requirements

--- src/medrecs.webapp/App_Start/IdentityConfig.cs
+++ src/medrecs.webapp/App_Start/IdentityConfig.cs
@@ -54,7 +54,7 @@ namespace medrecs.webapp
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
